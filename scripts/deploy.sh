baeeb523c7f19034227d75813aea51c6189bed73
hshockley <hshockley@rocketsoftware.com>
2020-06-26 13:25:07 -0400

    chore(project): revert deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,31 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-echo "The commit is a tag, publish to NPM!"
+# Git user info configs
+git config --global user.email "carbon@us.ibm.com"
+git config --global user.name "carbon-bot"
 
-# authenticate with the npm registry
-npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+# Add github token to git credentials
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
 
-node scripts/clean-package-jsons.js
+if [ -z "$TRAVIS_TAG" ]
+then
+	echo "The commit is not a tag, get lerna to version packages, and publish to Github."
 
-lerna publish from-git --yes --force-publish --contents dist
+	git stash
 
+	# checkout master to get out of detached HEAD state
+	git checkout master
+
+	lerna version --conventional-commits --yes --force-publish
+else
+	echo "The commit is a tag, publish to NPM!"
+
+	# authenticate with the npm registry
+	npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+
+	node scripts/clean-package-jsons.js
+
+	lerna publish from-git --yes --force-publish --contents dist
+fi
